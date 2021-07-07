859e17ee3acf812ec4a9652aabb31ad7725740ca
Iris <irisxia2016@gmail.com>
2021-07-07 16:12:56 +0800

    This time it worked

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,13 +6,13 @@ public class LeapYear {
 
     public static boolean isLeapYear(int year){
         if (year % 400 == 0){
-            return true
+            return true;
         }
-        elif (year % 4 == 0 && year % 100 != 0){
-            return true
+        else if ((year % 4 == 0) && (year % 100 != 0)){
+            return true;
         }
         else{
-            return false
+            return false;
         }
     }
      
